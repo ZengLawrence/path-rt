110b375b97496068e2ef151b2859eb88e0b4bef9
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-06 17:29:21 -0500

    Scaffold app to remove sample code and add bootstrap (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
