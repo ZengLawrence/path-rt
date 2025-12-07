189103644df2627808755bbea9c60767e5ed80f1
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-07 10:37:34 -0500

    integrate with ridepath api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import StationCard from './components/StationCard';
+import type { Station } from './models/Station';
+
+function parseStationData(data: any): Station[] {
+  return data.results.map((station: any) => ({
+    name: station.consideredStation,
+    trains: station.destinations.flatMap((destination: any) => (
+      destination.messages.map((message: any) => ({
+        headSign: message.headSign,
+        arrivalTimeMessage: message.arrivalTimeMessage,
+      }))
+    )),
+  }));
+}
 
 function App() {
+  const [stations, setStations] = useState<Station[]>([]);
+
+  useEffect(() => {
+    async function fetchStations() {
+      const options = { 
+        method: 'GET',
+        headers: { 
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+      };
+      const response = await fetch('bin/portauthority/ridepath.json', options);
+      const data = await response.json();
+      setStations(parseStationData(data));
+    }
+    fetchStations();
+  }, []);
+
   return (
     <Container>
       <h1 className="text-center">Real Time Train Departures</h1>
+      {stations.map((station, index) => (
+        <StationCard key={index} station={station} />
+      ))}
     </Container>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/bin/portauthority': {
+        target: 'https://www.panynj.gov',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
