d614d1253d4e640f116aeb5437b1a31d7420baa3
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-13 15:55:44 -0500

    fix lint error

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,7 @@ import type { Station } from "../models/Station";
 import stationsJson from "../models/stations.json";
 
 const stations: Record<string, { name: string } | null> = stationsJson;
+
 interface RidePathResponse {
   results: {
     consideredStation: string;
@@ -17,7 +18,7 @@ interface RidePathResponse {
 function parseStationData(data: RidePathResponse): Station[] {
   return data.results.map((station) => ({
     key: station.consideredStation,
-    name: stations[station.consideredStation]?.name || station.consideredStation,
+    name: stations[station.consideredStation]?.name ?? station.consideredStation,
     trains: station.destinations.flatMap((destination) => (
       destination.messages.map((message) => ({
         headSign: message.headSign,
