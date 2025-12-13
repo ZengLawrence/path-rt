a533713b530188af6ba7f3d85c70858170555857
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-13 09:19:20 -0500

    extract service to its own file to separate concerns

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,17 @@ import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import StationCard from './components/StationCard';
 import type { Station } from './models/Station';
-
-function parseStationData(data: any): Station[] {
-  return data.results.map((station: any) => ({
-    name: station.consideredStation,
-    trains: station.destinations.flatMap((destination: any) => (
-      destination.messages.map((message: any) => ({
-        headSign: message.headSign,
-        arrivalTimeMessage: message.arrivalTimeMessage,
-      }))
-    )),
-  }));
-}
-
-function filterNewportOrWTC(stations: Station[]): Station[] {
-  return stations.filter(station => station.name === "NEW" || station.name === "WTC");
-}
+import { fetchStations } from './services';
 
 function App() {
   const [stations, setStations] = useState<Station[]>([]);
 
   useEffect(() => {
-    async function fetchStations() {
-      const options = { 
-        method: 'GET',
-        headers: { 
-          'Accept': 'application/json'
-        }
-      };
-      const response = await fetch('bin/portauthority/ridepath.json', options);
-      const data = await response.json();
-      const stations = filterNewportOrWTC(parseStationData(data));
+    const loadStations = async () => {
+      const stations = await fetchStations();
       setStations(stations);
-    }
-    fetchStations();
+    };
+    loadStations();
   }, []);
 
   return (
