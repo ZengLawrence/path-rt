acde4b54762e413a5b0dade51faef0317b23a977
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-12-31 15:36:00 -0500

    Uninstall preview as it is crashing

--- __previewjs__/Wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export function Wrapper({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
-}
