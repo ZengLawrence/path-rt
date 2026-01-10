6f447db5ecdf0ab80244fbdabcaf83bb1616614e
Lawrence Zeng <lawrence.zeng@gmail.com>
2026-01-10 11:34:02 -0500

    Remove trip state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import { loadTripSelection, saveTripSelection } from './app/localStorage';
-import StationCard from './components/StationCard';
 import SelectTripForm from './components/SelectTripForm';
+import StationCard from './components/StationCard';
 import { useGeoLocation, useSchedule } from './hooks';
 import { addDistanceToLocation, allStationKeysAndNames, getStationName } from './models';
 import type { GeoCoordinate } from './models/GeoCoordinate';
@@ -47,41 +47,58 @@ function orderTripByLocation(selected: { key1: string, key2: string; lockDirecti
   }
 }
 
+function isStartingStation(station: { key: string }, trip: { from: string }) {
+  const { from } = trip;
+  return (from === "all" || station.key === from);
+};
+
+function destinationTargets({ from, to }: { from: string; to: string; }): DestinationTarget[] {
+  if (from == "all" || to == "all" || from == to) {
+    return [];
+  } else {
+    return getDestinationTargets(from, to, scheduleType(new Date()));
+  }
+}
+
+function limitToTargets(station: Station, destTargets: DestinationTarget[]): Station {
+  // no destination target, return all trains
+  if (destTargets.length == 0) return station;
+
+  const byTarget = (train: Train) => {
+    const targets = destTargets.map(segment => segment.target);
+    return targets.includes(train.target);
+  }
+  const targetedTrains = station.trains.filter(byTarget)
+  return { ...station, trains: targetedTrains };
+}
+
+function addStationTransferStation(station: Station, destTargets: DestinationTarget[]) {
+  const withTransferStation = (train: Train) => {
+    return addTransferStation(train, getTransferKey(destTargets, train.target));
+  }
+  const targetedTrains = station.trains.map(withTransferStation);
+  return { ...station, trains: targetedTrains };
+}
+
+
 function App() {
   const { stations, lastUpdated } = useSchedule();
 
   const stationsWithAllOption = [{ key: "all", name: "All" }].concat(allStationKeysAndNames().sort(byName));
   const [selectedStationKeys, setSelectedStationKeys] = useState(() => loadTripSelection());
   const location = useGeoLocation();
-  const [trip, setTrip] = useState(() => orderTripByLocation(selectedStationKeys, location));
-
-  useEffect(() => {
-    setTrip(orderTripByLocation(selectedStationKeys, location));
-  }, [selectedStationKeys, location]);
 
   const handleStationsChange = (selected: { key1: string, key2: string; lockDirection?: boolean; }) => {
     setSelectedStationKeys(selected);
     saveTripSelection(selected);
   };
 
-  const byStartingStation = (station: { key: string }) => {
-    const { from } = trip;
-    return (from === "all" || station.key === from);
-  };
-
-  const withTargets = (station: Station) => {
-    const { from, to } = trip;
-    if (from == "all" || to == "all" || from == to) {
-      return station;
-    } else {
-      const destTargets = getDestinationTargets(from, to, scheduleType(new Date()));
-      const targets = destTargets.map(segment => segment.target);
-      const targetedTrains = station.trains
-        .filter(train => targets.includes(train.target))
-        .map(train => addTransferStation(train, getTransferKey(destTargets, train.target)));
-      return { ...station, trains: targetedTrains };
-    }
-  }
+  const trip = orderTripByLocation(selectedStationKeys, location);
+  const destTargets = destinationTargets(trip);
+  const displayedStations = stations.sort(byName)
+    .filter(station => isStartingStation(station, trip))
+    .map(station => limitToTargets(station, destTargets))
+    .map(station => addStationTransferStation(station, destTargets));
 
   return (
     <Container>
@@ -92,12 +109,9 @@ function App() {
         onChange={handleStationsChange}
       />
       <p className='mt-2'>Last updated: {lastUpdated ? lastUpdated.toLocaleString() : "Pending..."}</p>
-      {stations.sort(byName)
-        .filter(byStartingStation)
-        .map(withTargets)
-        .map((station) => (
-          <StationCard key={station.key} station={station} />
-        ))}
+      {displayedStations.map((station) => (
+        <StationCard key={station.key} station={station} />
+      ))}
     </Container>
   )
 }
